71d0262ab52ee7b39e0049b34a942fa565782af5
chinmaya-elred <chinmaya@elred.io>
2024-07-09 13:59:27 +0530

    completed till section 2

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <div>
       <BasicsOfMotion />
-      {/* <Gestures /> */}
-      {/* <AnimationControls /> */}
-      {/* <ViewBasedAnimations /> */}
-      {/* <ScrollAnimations /> */}
+      <Gestures />
+      <AnimationControls />
+      <ViewBasedAnimations />
+      <ScrollAnimations />
     </div>
   );
 }
